420bd09cde8e4636007b0908c789d74b4aa2d7aa
alexisperaza <alexisperaza54@gmail.com>
2022-07-24 18:46:43 -0600

    entregable 1

---
+++ Entregable1/ApellidoNombre.js
@@ -0,0 +1,44 @@
+class Usuario {
+    constructor(nombre, apellido){
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.libros = [];
+        this.mascotas = [];
+    }
+
+    getFullName = () => {
+        return `${this.nombre} ${this.apellido}`
+    }
+    addMascota = (mascota) => {
+        this.mascotas.push(mascota)
+    }
+    countMascotas = () => {
+        return this.mascotas.length
+    }
+    addBook = (nombre, autor) => {
+        this.libros.push({nombre: nombre, autor: autor})
+    }
+    getBookNames = () => {
+        let nombres = []
+        this.libros.forEach((libro) => nombres.push(libro.nombre))
+        return nombres
+    }
+
+
+
+}    
+
+let persona = new Usuario("Alexis", "Peraza");
+
+console.log(persona.getFullName());
+
+persona.addMascota('Ted')
+persona.addMascota('Coco')
+persona.addMascota('Max')
+
+console.log(persona.countMascotas());
+
+persona.addBook('Harry Potter', 'JK Collins')
+persona.addBook('Cien años de soledad', 'Garcia Marquez')
+
+console.log(persona.getBookNames());
